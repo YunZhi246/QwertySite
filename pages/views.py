29795299a9a92f93a2557c34cd8d3c25e92a47c2
Yun Zhi Lin <yunzhi246@gmail.com>
2018-01-24 18:11:40 -0500

    Created recruitment form

--- pages/views.py
+++ pages/views.py
@@ -16,8 +16,13 @@ def home(request):
 def aboutUs(request):
   return render(request, 'pages/about_us.html')
 
-def recruit(request):
-  return render(request, 'pages/recruitment.html')
+def recruit(request, msg=False):
+  context = {}
+  if msg == "sent":
+    context = {
+      'msg': 'Application sent!',
+    }  
+  return render(request, 'pages/recruitment.html', context)
 
 def contactUs(request, msg=False):
   context = {}
@@ -44,3 +49,36 @@ def contactForm(request):
   email.attach_alternative(htmlContent, "text/html")
   email.send(fail_silently=False)
   return HttpResponseRedirect(reverse('pages:contactUs', args=("sent", )))
+
+def recruitForm(request):
+  nameEmail = 'Name: \n' + request.POST['name'] + '\n\nEmail: \n' + request.POST['email']
+  posiExper = '\n\nDesired Position: \n' + request.POST['position'] + '\n\nDo you have experience with the position you selected? \n' + request.POST['experience']
+  groups = '\n\nAre you in any other groups? If yes, please list them below. If no, skip this question. \n' + request.POST['groups']
+  proj = '\n\nWhat project are you interested in working on? \n' + request.POST['projects']
+  selfIntro = '\n\nTell us a bit about yourself. \n' + request.POST['selfintro']
+  discov = '\n\nHow did you discover us? \n' + request.POST['discovery']
+  reason = '\n\nWhy do you want to join our team? \n'  + request.POST['reason']
+  comment = '\n\nIs there anything else you want to tell us? Questons, comments, past experience, whether or not you are applying for another position.\n' + request.POST['comment']
+  emailContent = nameEmail + posiExper + proj + selfIntro + discov + reason + comment 
+  
+  hnameEmail = '<b>Name:</b> ' + request.POST['name'] + '<br/><br/><b>Email:</b> ' + request.POST['email']
+  hposiExper = '<br/><br/><b>Desired Position: </b>' + request.POST['position'] + '<br/><br/><b>Do you have experience with the position you selected? </b>' + request.POST['experience']
+  hgroups = '<br/><br/><b>Are you in any other groups? If yes, please list them below. If no, skip this question. </b>' + request.POST['groups']
+  hproj = '<br/><br/><b>What project are you interested in working on? </b>' + request.POST['projects']
+  hselfIntro = '<br/><br/><b>Tell us a bit about yourself. </b>' + request.POST['selfintro']
+  hdiscov = '<br/><br/><b>How did you discover us? </b>' + request.POST['discovery']
+  hreason = '<br/><br/><b>Why do you want to join our team? </b>'  + request.POST['reason']
+  hcomment = '<br/><br/><b>Is there anything else you want to tell us? Questons, comments, past experience, whether or not you are applying for another position.</b>' + request.POST['comment']  
+  htmlContent = hnameEmail + hposiExper + hproj + hselfIntro + hdiscov + hreason + hcomment 
+  subject = 'New Application from ' +  request.POST['name']
+  replyToEmail = request.POST['email']
+  email = EmailMultiAlternatives(
+             subject,
+             emailContent,
+             'from@example.com',
+             ['to@example.com'],
+             reply_to=[replyToEmail],
+           )  
+  email.attach_alternative(htmlContent, "text/html")
+  email.send(fail_silently=False)
+  return HttpResponseRedirect(reverse('pages:recruit', args=("sent", )))
